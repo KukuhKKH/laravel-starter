495d03ea86b08b073eed124882ec65123f6f1dc7
Kukuh Rahmadani <krahmadani1@gmail.com>
2021-12-26 08:45:54 +0700

    Add Inertia.js

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -37,6 +37,10 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+
+            /** Innertia Middleware (Optional) */
+            \App\Http\Middleware\HandleInertiaRequests::class,
+            /** Innertia Middleware (Optional) */
         ],
 
         'api' => [
