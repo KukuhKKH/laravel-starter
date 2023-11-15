4ace64cd949bd43e2aacf100b6646edbf5e9282d
BangLipai <developer.kukuh@gmail.com>
2023-11-15 21:56:36 +0700

    Generate model dan penyesuaian migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,14 +3,58 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Carbon\Carbon;
+use Eloquent;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Yajra\Auditable\AuditableWithDeletesTrait;
 
+/**
+ * App\Models\User
+ * @mixin Eloquent
+ *
+ * @property int         $id
+ * @property string      $name
+ * @property string      $email
+ * @property null|Carbon $email_verified_at
+ * @property string      $password
+ * @property null|string $remember_token
+ * @property null|Carbon $created_at
+ * @property null|Carbon $updated_at
+ * @property null|Carbon $deleted_at
+ * @property null|int    $created_by
+ * @property null|int    $updated_by
+ * @property null|int    $deleted_by
+ */
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use AuditableWithDeletesTrait;
+    use HasApiTokens;
+    use HasFactory;
+    use Notifiable;
+    use SoftDeletes;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'users';
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'name'              => 'string',
+        'email'             => 'string',
+        'email_verified_at' => 'datetime',
+        'password'          => 'hashed',
+    ];
 
     /**
      * The attributes that are mass assignable.
@@ -31,15 +75,5 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
         'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-        'password' => 'hashed',
     ];
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,10 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+
             $table->timestamps();
+            $table->softDeletes();
+            $table->auditableWithDeletes();
         });
     }
 
