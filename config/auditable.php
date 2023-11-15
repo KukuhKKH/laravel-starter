9d4fc7626863bfcd9b88d80114c592e2f3e529e7
BangLipai <developer.kukuh@gmail.com>
2023-11-15 21:53:16 +0700

    Install package

---
+++ config/auditable.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'defaults' => [
+        'creator' => [
+            'name' => '',
+            // add creator default values if not set
+        ],
+
+        'updater' => [
+            'name' => '',
+            // add updater default values if not set
+        ],
+
+        'deleter' => [
+            'name' => '',
+            // add deleter default values if not set
+        ],
+    ],
+];
