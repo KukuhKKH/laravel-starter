e657c4b28d59024085e3136694f18881407c89be
Kukuh Rahmadani <krahmadani1@gmail.com>
2024-01-11 22:33:19 +0700

    Middleware akses dengan user grup dan user akses

--- app/Http/Middleware/Akses.php
+++ app/Http/Middleware/Akses.php
@@ -3,11 +3,15 @@
 namespace App\Http\Middleware;
 
 use App\Models\RouteAkses;
+use App\Models\User;
 use App\Services\AksesService;
 use App\Services\AkunService;
 use Closure;
 use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Gate;
 use Symfony\Component\HttpFoundation\Response;
 
 class Akses
@@ -30,8 +34,28 @@ class Akses
             ->where('route_key', '=', $key)
             ->first();
 
-        if ($routeAkses && !$this->aksesService->isAkses(user(), $routeAkses->k_akses)) {
-            throw new AuthorizationException("Akun tidak memiliki akses {$routeAkses->route->label}");
+        $user       = user();
+        $groupAkses = $this->akunService->getGrupAkses($user);
+        $userAkses  = $this->aksesService->getUserAkses($user, $groupAkses);
+
+        if ($routeAkses && !$this->aksesService->isAkses($userAkses, $routeAkses->k_akses)) {
+            throw new AuthorizationException("Akun tidak memiliki akses");
+        }
+
+        if ($routeAkses) {
+            foreach ($request->route()->parameters() as $model) {
+
+                if (!($model instanceof Model) || !method_exists($model, 'getPolicyName')) {
+                    continue;
+                }
+
+                if ($ability = $model->getPolicyName($routeAkses->route->key)) {
+                    $inspect = Gate::inspect($ability, $model);
+                    if ($inspect->denied()) {
+                        throw new AuthorizationException($inspect->message());
+                    }
+                }
+            }
         }
 
         return $next($request);
