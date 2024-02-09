b2842d794fe445556a8540434b0d6b666f096104
Bang Lipai <developer.kukuh@gmail.com>
2024-02-09 11:01:59 +0700

    fix: Builder master column

--- app/Providers/MigrationServiceProvider.php
+++ app/Providers/MigrationServiceProvider.php
@@ -17,11 +17,16 @@ class MigrationServiceProvider extends ServiceProvider
             /** @var Blueprint $this */
             $table = $this;
 
+            $nullSingkat    = (bool)Arr::get($nullable, 'singkat', false);
+            $nullKeterangan = (bool)Arr::get($nullable, 'keterangan', false);
+
             $table->string('singkat', $singkat)
-                ->nullable(Arr::get($nullable['singkat'], true));
+                ->nullable($nullSingkat);
 
             $table->string('keterangan', $keterangan)
-                ->nullable(Arr::get($nullable['keterangan'], true));
+                ->nullable($nullKeterangan);
+
+            return $this;
         });
     }
 
