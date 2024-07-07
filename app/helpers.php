ed5c2374d2e94d04fad734639a271e26babbe24b
Bang Lipai <developer.kukuh@gmail.com>
2024-07-07 20:50:21 +0700

    Perampingan laravel

--- app/helpers.php
+++ app/helpers.php
@@ -5,16 +5,11 @@ use Illuminate\Http\JsonResponse;
 
 function user(string $guard = null): ?User
 {
-    /** @var User|null */
+    /** @var ?User */
     return Auth::guard($guard ?: config('auth.defaults.guard'))->user();
 }
 
 function userId(string $guard = null): ?int
 {
     return user($guard)?->id;
-}
-
-function success($data = null, $status = 200, $code = null, $meta = null): JsonResponse
-{
-    return Response::success($data, $status, $code, $meta);
 }
