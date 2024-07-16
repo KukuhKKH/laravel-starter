6eb883c653d9d04118d8d49e8e62a87332f6a07d
Bang Lipai <developer.kukuh@gmail.com>
2024-07-16 20:38:21 +0700

    Package auditable

--- _ide_helper.php
+++ _ide_helper.php
@@ -18083,6 +18083,230 @@ namespace Illuminate\Support\Facades {
             }
     }
 
+namespace Clockwork\Support\Laravel {
+            /**
+     * 
+     *
+     */        class Facade {
+                    /**
+         * 
+         *
+         * @static 
+         */        public static function addDataSource($dataSource)
+        {
+                        /** @var \Clockwork\Clockwork $instance */
+                        return $instance->addDataSource($dataSource);
+        }
+                    /**
+         * 
+         *
+         * @static 
+         */        public static function resolveRequest()
+        {
+                        /** @var \Clockwork\Clockwork $instance */
+                        return $instance->resolveRequest();
+        }
+                    /**
+         * 
+         *
+         * @static 
+         */        public static function resolveAsCommand($name, $exitCode = null, $arguments = [], $options = [], $argumentsDefaults = [], $optionsDefaults = [], $output = null)
+        {
+                        /** @var \Clockwork\Clockwork $instance */
+                        return $instance->resolveAsCommand($name, $exitCode, $arguments, $options, $argumentsDefaults, $optionsDefaults, $output);
+        }
+                    /**
+         * 
+         *
+         * @static 
+         */        public static function resolveAsQueueJob($name, $description = null, $status = 'processed', $payload = [], $queue = null, $connection = null, $options = [])
+        {
+                        /** @var \Clockwork\Clockwork $instance */
+                        return $instance->resolveAsQueueJob($name, $description, $status, $payload, $queue, $connection, $options);
+        }
+                    /**
+         * 
+         *
+         * @static 
+         */        public static function resolveAsTest($name, $status = 'passed', $statusMessage = null, $asserts = [])
+        {
+                        /** @var \Clockwork\Clockwork $instance */
+                        return $instance->resolveAsTest($name, $status, $statusMessage, $asserts);
+        }
+                    /**
+         * 
+         *
+         * @static 
+         */        public static function extendRequest($request = null)
+        {
+                        /** @var \Clockwork\Clockwork $instance */
+                        return $instance->extendRequest($request);
+        }
+                    /**
+         * 
+         *
+         * @static 
+         */        public static function storeRequest()
+        {
+                        /** @var \Clockwork\Clockwork $instance */
+                        return $instance->storeRequest();
+        }
+                    /**
+         * 
+         *
+         * @static 
+         */        public static function reset()
+        {
+                        /** @var \Clockwork\Clockwork $instance */
+                        return $instance->reset();
+        }
+                    /**
+         * 
+         *
+         * @static 
+         */        public static function request($request = null)
+        {
+                        /** @var \Clockwork\Clockwork $instance */
+                        return $instance->request($request);
+        }
+                    /**
+         * 
+         *
+         * @static 
+         */        public static function log($level = null, $message = null, $context = [])
+        {
+                        /** @var \Clockwork\Clockwork $instance */
+                        return $instance->log($level, $message, $context);
+        }
+                    /**
+         * 
+         *
+         * @static 
+         */        public static function timeline()
+        {
+                        /** @var \Clockwork\Clockwork $instance */
+                        return $instance->timeline();
+        }
+                    /**
+         * 
+         *
+         * @static 
+         */        public static function event($description, $data = [])
+        {
+                        /** @var \Clockwork\Clockwork $instance */
+                        return $instance->event($description, $data);
+        }
+                    /**
+         * 
+         *
+         * @static 
+         */        public static function shouldCollect($shouldCollect = null)
+        {
+                        /** @var \Clockwork\Clockwork $instance */
+                        return $instance->shouldCollect($shouldCollect);
+        }
+                    /**
+         * 
+         *
+         * @static 
+         */        public static function shouldRecord($shouldRecord = null)
+        {
+                        /** @var \Clockwork\Clockwork $instance */
+                        return $instance->shouldRecord($shouldRecord);
+        }
+                    /**
+         * 
+         *
+         * @static 
+         */        public static function dataSources($dataSources = null)
+        {
+                        /** @var \Clockwork\Clockwork $instance */
+                        return $instance->dataSources($dataSources);
+        }
+                    /**
+         * 
+         *
+         * @static 
+         */        public static function storage($storage = null)
+        {
+                        /** @var \Clockwork\Clockwork $instance */
+                        return $instance->storage($storage);
+        }
+                    /**
+         * 
+         *
+         * @static 
+         */        public static function authenticator($authenticator = null)
+        {
+                        /** @var \Clockwork\Clockwork $instance */
+                        return $instance->authenticator($authenticator);
+        }
+                    /**
+         * 
+         *
+         * @static 
+         */        public static function getDataSources()
+        {
+                        /** @var \Clockwork\Clockwork $instance */
+                        return $instance->getDataSources();
+        }
+                    /**
+         * 
+         *
+         * @static 
+         */        public static function getRequest()
+        {
+                        /** @var \Clockwork\Clockwork $instance */
+                        return $instance->getRequest();
+        }
+                    /**
+         * 
+         *
+         * @static 
+         */        public static function setRequest($request)
+        {
+                        /** @var \Clockwork\Clockwork $instance */
+                        return $instance->setRequest($request);
+        }
+                    /**
+         * 
+         *
+         * @static 
+         */        public static function getStorage()
+        {
+                        /** @var \Clockwork\Clockwork $instance */
+                        return $instance->getStorage();
+        }
+                    /**
+         * 
+         *
+         * @static 
+         */        public static function setStorage($storage)
+        {
+                        /** @var \Clockwork\Clockwork $instance */
+                        return $instance->setStorage($storage);
+        }
+                    /**
+         * 
+         *
+         * @static 
+         */        public static function getAuthenticator()
+        {
+                        /** @var \Clockwork\Clockwork $instance */
+                        return $instance->getAuthenticator();
+        }
+                    /**
+         * 
+         *
+         * @static 
+         */        public static function setAuthenticator($authenticator)
+        {
+                        /** @var \Clockwork\Clockwork $instance */
+                        return $instance->setAuthenticator($authenticator);
+        }
+            }
+    }
+
 namespace Illuminate\Http {
             /**
      * 
@@ -18170,6 +18394,42 @@ namespace Illuminate\Database\Schema {
          */        public static function masterColumn($singkat = 50, $keterangan = 100, $nullable = [])
         {
                         return \Illuminate\Database\Schema\Blueprint::masterColumn($singkat, $keterangan, $nullable);
+        }
+                    /**
+         * 
+         *
+         * @see \Yajra\Auditable\AuditableServiceProvider::register()
+         * @static 
+         */        public static function auditable()
+        {
+                        return \Illuminate\Database\Schema\Blueprint::auditable();
+        }
+                    /**
+         * 
+         *
+         * @see \Yajra\Auditable\AuditableServiceProvider::register()
+         * @static 
+         */        public static function dropAuditable()
+        {
+                        return \Illuminate\Database\Schema\Blueprint::dropAuditable();
+        }
+                    /**
+         * 
+         *
+         * @see \Yajra\Auditable\AuditableServiceProvider::register()
+         * @static 
+         */        public static function auditableWithDeletes()
+        {
+                        return \Illuminate\Database\Schema\Blueprint::auditableWithDeletes();
+        }
+                    /**
+         * 
+         *
+         * @see \Yajra\Auditable\AuditableServiceProvider::register()
+         * @static 
+         */        public static function dropAuditableWithDeletes()
+        {
+                        return \Illuminate\Database\Schema\Blueprint::dropAuditableWithDeletes();
         }
             }
     }
@@ -21883,6 +22143,7 @@ namespace  {
             class Validator extends \Illuminate\Support\Facades\Validator {}
             class View extends \Illuminate\Support\Facades\View {}
             class Vite extends \Illuminate\Support\Facades\Vite {}
+            class Clockwork extends \Clockwork\Support\Laravel\Facade {}
     }
 
 
--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,7 +18,10 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+
             $table->timestamps();
+            $table->softDeletes();
+            $table->auditableWithDeletes();
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
